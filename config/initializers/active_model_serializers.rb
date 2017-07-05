5d8d33175a3c0d1f425cd4d3d861e76caf0a3d19
lottetreg <cjtregelles@email.wm.edu>
2017-07-05 11:12:56 -0400

    use json instead of jsonapi for now, until we set up jsonapi parsing on shop

--- config/initializers/active_model_serializers.rb
+++ config/initializers/active_model_serializers.rb
@@ -2,5 +2,5 @@ require 'active_model_serializers'
 
 # Set config in host app unless host is not using AMS or absolutely sure it wont change/break host serialization
 
-ActiveModelSerializers.config.adapter = :json_api
+ActiveModelSerializers.config.adapter = :json
 ActiveModelSerializers.config.key_transform = :underscore
